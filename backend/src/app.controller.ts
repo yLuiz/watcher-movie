a190ec23b83367f9541031d8dfaaf1f22f10e9ac
yluiz <luizvictor1231@gmail.com>
2024-02-12 14:00:58 -0400

    refactor: make getHello public

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from './auth/public';
 @ApiTags('Hello')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
